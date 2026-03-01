e7d8ee3f5e99ec23288fa66fc24b7e8ea9668c11
ivelascocode <isaidvm@email.com>
2026-02-28 22:00:05 -0600

    Fix: path to config.php using __DIR__

--- MensajeSTT.php
+++ MensajeSTT.php
@@ -1,7 +1,7 @@
 <?php
 // filepath: /Applications/MAMP/htdocs/tt/MensajeSTT.php
 header('Content-Type: text/html; charset=utf-8');
-require_once '/../inc/config.php';
+require_once '/inc/config.php';
 
 try {
     $conn = conect();
